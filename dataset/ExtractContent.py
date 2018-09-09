974e7ec6a647c7c59a7bd7d78f6b972c1fd0ea2e
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2018-09-09 14:21:41 +0700

    Modified ExtractContent

--- dataset/ExtractContent.py
+++ dataset/ExtractContent.py
@@ -3,12 +3,14 @@
 # and store it in a new file with the same name in the dst dir. 
 
 import email.parser 
+import glob
 import os, sys, stat
+import re
 import shutil
 
 def ExtractSubPayload (filename):
-	''' Extract the subject and payload from the .eml file.
-	
+	''' 
+	Extract the subject and payload from the .eml file.
 	'''
 	if not os.path.exists(filename): # dest path doesnot exist
 		print "ERROR: input file does not exist:", filename
@@ -23,14 +25,14 @@ def ExtractSubPayload (filename):
 	if type(payload) != type('') :
 		payload = str(payload)
 	
-	return sub + payload
+	return sub + '\n' + payload
 
 def ExtractBodyFromDir ( srcdir, dstdir ):
 	'''Extract the body information from all .eml files in the srcdir and 
 	
 	save the file to the dstdir with the same name.'''
 	if not os.path.exists(dstdir): # dest path doesnot exist
-		os.makedirs(dstdir)  
+		os.makedirs(dstdir)
 	files = os.listdir(srcdir)
 	for file in files:
 		srcpath = os.path.join(srcdir, file)
@@ -48,17 +50,17 @@ def ExtractBodyFromDir ( srcdir, dstdir ):
 ###################################################################
 # main function start here
 # srcdir is the directory where the .eml are stored
-print 'Input source directory: ' #ask for source and dest dirs
-srcdir = raw_input()
+# print 'Input source directory: ' #ask for source and dest dirs
+srcdir = 'TR-mails/TR-raw/'
 if not os.path.exists(srcdir):
 	print 'The source directory %s does not exist, exit...' % (srcdir)
 	sys.exit()
 # dstdir is the directory where the content .eml are stored
-print 'Input destination directory: ' #ask for source and dest dirs
-dstdir = raw_input()
+# print 'Input destination directory: ' #ask for source and dest dirs
+dstdir = 'TR-mails/TR-processed/'
 if not os.path.exists(dstdir):
 	print 'The destination directory is newly created.'
 	os.makedirs(dstdir)
 
 ###################################################################
-ExtractBodyFromDir ( srcdir, dstdir ) 
+ExtractBodyFromDir ( srcdir, dstdir ) beautifulsoup4
