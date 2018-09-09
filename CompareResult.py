694529f600a7b8b02c28f222fc603b712d96aa46
Wenny Yustalim <wennyyustalim@gmail.com>
2018-09-09 17:34:51 +0700

    Add script to compare 2 files and output to difference.txt

---
+++ CompareResult.py
@@ -0,0 +1,18 @@
+# Compare 2 files and output the difference to difference.txt
+
+def compare(File1,File2):
+    with open(File1,'r') as f:
+        d=set(f.readlines())
+
+
+    with open(File2,'r') as f:
+        e=set(f.readlines())
+	
+	# Create file
+    open('difference.txt','w').close()
+
+    with open('difference.txt','a') as f:
+        for line in list(d-e):
+           f.write(line)
+
+compare("dataset/spam-mail.tr.label","dataset/result.txt")
