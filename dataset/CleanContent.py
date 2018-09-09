04f2a171bca0e071f8d8f8db0b9f36964d1db2b5
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2018-09-09 15:34:04 +0700

    Modify CleanContent to use contractions

--- dataset/CleanContent.py
+++ dataset/CleanContent.py
@@ -1,19 +1,23 @@
+import contractions
 import glob
-import nltk
-import os
-import re
 
 from bs4 import BeautifulSoup
 
-for filepath in glob.glob('TR-mails/TR-processed/*'):
-    new_lines = []
+def clean_data(strip_html_tags=True, extract_contractions=True):
+    for filepath in glob.glob('TR-mails/TR-processed/*'):
+        new_lines = []
 
-    infile = open(filepath, 'rb')
-    for line in infile:
-        cleantext = BeautifulSoup(line, 'html.parser').text
-        new_lines.append(cleantext)
+        infile = open(filepath, 'rb')
+        for line in infile:
+            if strip_html_tags:
+                line = BeautifulSoup(line, 'html.parser').text
+            new_lines.append(line)
 
-    outfile = open(filepath, 'w+')
-    for line in new_lines:
-        if line != '\n':
-            outfile.write(line)
+        outfile = open(filepath, 'w+')
+        for line in new_lines:
+            if line != '\n':
+                if extract_contractions:
+                    line = contractions.fix(line)
+                outfile.write(line)
+
+clean_data()
